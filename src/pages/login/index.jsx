f01033cf0645ffbabe8f95b78b07520abc7402f6
yangchao <1293114597@qq.com>
2019-06-23 13:16:57 +0800

    表单校验

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -11,6 +11,44 @@ class Login extends Component {
   login = (e) => {
     e.preventDefault();
 
+    //用来校验表单并获取表单的值
+    this.props.form.validateFields((error, valuses) => {
+      //console.log(error, valuses);
+      /*
+        error 代表表单校验结果
+        null 校验通过
+        {}检验失败
+      */
+      if (!error) {
+        //校验通过
+        const { username, password } = valuses;
+
+        //发送请求，请求登录
+        console.log(username,password);
+      }else {
+        console.log('登录表单校验失败：', error);
+      }
+    })
+  }
+
+  validator = (rule, value, callback) =>{
+    //callback 必须调用
+    // console.log(rule,value);
+
+    const name = rule.fullField === 'username' ? '用户名' : '密码';
+
+    if (!value) {
+      callback(`必须输入${name}!`);
+    }else if (value.length < 4) {
+      callback(`${name}必须大于4位`);
+    }else if (value.length > 16) {
+      callback(`${name}必须小于16位`);
+    }else if (!/^[a-zA-Z_0-9]+$/.test(value)) {
+      callback(`${name}只能包含英文字母、数字和下划线`);
+    }else {
+      //不传参代表校验通过，传参代表校验失败。
+      callback();
+    }
   }
 
   render() {
@@ -31,10 +69,13 @@ class Login extends Component {
                 'username',
                 {
                   rules: [
-                    {required: true, message: '请输入用户名！'},
+                  /*  {required: true, message: '请输入用户名！'},
                     {min: 4, message: '用户名必须大于4位'},
                     {max: 16, message: '用户名必须小于16位'},
-                    {pattern: /^[a-zA-Z_0-9]+$/, message: '用户名只能包含英文字母、数字和下划线'}
+                    {pattern: /^[a-zA-Z_0-9]+$/, message: '用户名只能包含英文字母、数字和下划线'} */
+                    {
+                      validator: this.validator
+                    }
                   ]
                 }
               )(
@@ -43,7 +84,20 @@ class Login extends Component {
             }
           </Item>
           <Item>
-            <Input className="login-input" prefix={<Icon type="lock" />} placeholder="密码" type="password"/>
+            {
+              getFieldDecorator(
+                'password',
+                {
+                  rules: [
+                    {
+                      validator: this.validator
+                    }
+                  ]
+                }
+              )(
+                <Input className="login-input" prefix={<Icon type="lock"/>} placeholder="密码" type="password"/>
+              )
+            }
           </Item>
           <Item>
             <Button type="primary" htmlType="submit" className="login-btn">登录</Button>
